04b52c022f9ebbcfa9d081091f14b6fd8787add8
Andres Castro <andres.ca@gmail.com>
2016-08-26 16:48:56 -0600

    Add tmux plugins and themes

--- install/makesymlinks.sh
+++ install/makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile bash_prompt functions aliases dircolors gitconfig vimrc vim tmux.conf"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile bash_prompt functions aliases dircolors gitconfig vimrc vim tmux.conf tmux"    # list of files/folders to symlink in homedir
 
 ##########
 
---
+++ tmux/config/human.sh
@@ -0,0 +1,1 @@
+/Users/crac/dotfiles
