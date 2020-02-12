e3c9596dde2cd6a47e14ab07e05814d0ef99da55
cracdev <andres.ca@gmail.com>
2020-02-12 12:43:28 -0600

    update tmux host by batery life

--- tmux/config/theme.sh
+++ tmux/config/theme.sh
@@ -56,6 +56,7 @@ tm_date="#[fg=$tm_color_inactive] %R %d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
 
-set -g status-left $tm_session_name' '
+set -g status-left '@crac  '
+#set -g status-left $tm_session_name' '
 set -g status-right $tm_date' '$tm_host
 
